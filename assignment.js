31cff45d7eda319157e4c61a5574d9b96cadb45e
raghavareddy <raghavareddy2325@gmail.com>
2022-08-17 00:08:58 +0530

    code added

---
+++ assignment.js
@@ -0,0 +1,45 @@
+// assigning add
+var a = 10;
+var b = 10;
+a += b;
+console.log(a);
+
+// assigning sub
+var b = 10;
+var a = 20;
+a -= b;
+console.log(a);
+
+// assigning mul
+var b = 10;
+var a = 10;
+a *= b;
+console.log(a);
+
+// assigning div
+var b = 10;
+var a = 10;
+a /= b;
+console.log(a);
+
+// assigning mod
+var b = 10;
+var a = 10;
+a %= b;
+console.log(b);
+
+// assigning bitwise
+var x = true;
+var y = false;
+x &= y
+console.log(x);
+
+var x1 = true;
+var y1 = false;
+x1 ^= y1
+console.log(x);
+
+var x2 = true;
+var y2 = false;
+x2 |= y2
+console.log(x);
